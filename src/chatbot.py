efbe6f7f2986ebc35db589d1f8afb41d385bbc6e
Ujan Pradhan <113442287+2003UJAN@users.noreply.github.com>
2025-03-30 05:47:25 +0530

    Update chatbot.py

--- src/chatbot.py
+++ src/chatbot.py
@@ -1,1 +1,11 @@
+from langchain.chains import ConversationChain
+from langchain.memory import ConversationBufferMemory
+from langchain_community.llms import HuggingFaceEndpoint
 
+def create_chatbot():
+    llm = HuggingFaceEndpoint(
+        repo_id="mistralai/Mistral-7B-Instruct-v0.2",
+        temperature=0.5
+    )
+    memory = ConversationBufferMemory()
+    return ConversationChain(llm=llm, memory=memory)
