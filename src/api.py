a4883d072307f972b1d7afceb787999129bbfb43
Ujan Pradhan <113442287+2003UJAN@users.noreply.github.com>
2025-03-30 05:48:40 +0530

    Update api.py

--- src/api.py
+++ src/api.py
@@ -1,1 +1,11 @@
+import streamlit as st
 
+APIS = {
+    'ride_hailing': {
+        'base_url': 'https://uber-ride-api.p.rapidapi.com/',
+        'headers': {'X-RapidAPI-Key': st.secrets["RAPIDAPI_KEY"]}
+    },
+    'ecommerce': {
+        'fake_store': 'https://fakestoreapi.com/'
+    }
+}
